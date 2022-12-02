9dfbe8ffd9e34403bebcb2bf5af03d1988b7214f
OwenCYork <ogc514@york.ac.uk>
2022-12-02 11:57:30 +0000

    Organised Day 2 into the 2 parts

--- 2022/day2.py
+++ 2022/day2.py
@@ -3,22 +3,25 @@ total = 0
 part2total = 0
 playerChoice = ["X","Y","Z"]
 adversaryChoice = ["A","B","C"]
+
 for i in rounds:
+    #Part 1
     round = i.strip("\n")
     if adversaryChoice.index(round[0])== ((playerChoice.index(round[2])-1)%3):
         total += 6
-
     elif adversaryChoice.index(round[0]) == playerChoice.index(round[2]):
         total += 3
-    
+    total += playerChoice.index(round[2])+1
+
+    #Part 2
     if round[0] == "A":
         part2total += ((playerChoice.index(round[2])-1)%3)+1
     elif round[0] == "B":
         part2total += playerChoice.index(round[2])+1
     else:
         part2total += ((playerChoice.index(round[2])+1)%3)+1
-    total += playerChoice.index(round[2])+1
     part2total += playerChoice.index(round[2])*3
 rounds.close()
+
 print(total)
 print(part2total)
